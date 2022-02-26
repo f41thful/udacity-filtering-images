6ccab1984da43ba86a15a4eed7a039b519e930c2
personal <personal@nb-jmediavilla.local>
2022-02-26 18:47:29 +0100

    Modifies configuration

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -5,11 +5,18 @@ export const config = {
     "database": process.env.DATABASE,
     "host": process.env.HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "user179-test-bucket"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.PROFILE,
+    "aws_media_bucket": process.env.BUCKET
   },
   "jwt": {
     "secret": "secret"
   }
 }
+
+console.log("Using username: " + config.dev.username)
+console.log("Using host: " + config.dev.host)
+console.log("Using database: " + config.dev.database)
+console.log("Using region: " + config.dev.aws_region)
+console.log("Using profile: " + config.dev.aws_profile)
+console.log("Using bucket: " + config.dev.aws_media_bucket)
